87948ea3005a331604fc9acfefc43d10d141d050
sparship <sparship@bnz-power.com>
2019-10-02 19:46:18 +0200

    some segment definitions added

---
+++ segments.js
@@ -0,0 +1,35 @@
+let segments = {
+    HKIPS: {
+        gvName: "SEPA Instant Payment Statusabfrage",
+        v4: "noch offen"
+    },
+    HKMTA: {
+        gvName: "Mobilfunkverbindung ändern", 
+        v4: "ChangeMobilePhoneConnection"
+    }, HKMTF: {
+        gvName: "Mobilfunkverbindung freischalten",
+        v4: "ActivateMobilePhoneConnection"
+    }, HKMTR: {
+        gvName: "Mobilfunkverbindung registrieren",
+        v4: "RegisterMobilePhoneConnection",
+        _note: "v4 typo in document - RegisterMobilePhoneConection"
+    }, HKPAE: {
+        gvName: "PIN ändern",
+        v4: "ChangePIN"
+    }, HKPSA: {
+        gvName: "PIN-Sperre aufheben",
+        v4: "RevokePINBlock"
+    }, HKPSP: {
+        gvName: "PIN-Sperre",
+        v4: "BlockPIN"
+    }, HKTAB: {
+        gvName: "TAN-Generator anzeigen, Bestand",
+        v4: "DisplayTANGeneratorList"
+    }, HKTAU: {
+        gvName: "TAN-Generator an-bzw. ummelden",
+        v4: "ChangeTANGenerator"
+    }, HKTSY: {
+        gvName: "TAN-Generator Synchronisierung",
+        v4: "SynchronizeTANGenerator"
+    }
+};
